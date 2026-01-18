6cdb908bb1e524c0e5da60e26707357202e79dc8
Harsh Kushwaha <harshkushwaha9695@gmail.com>
2026-01-18 15:15:42 +0530

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  // TEMP: load a public website
+  // Later we will load YOUR website
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
